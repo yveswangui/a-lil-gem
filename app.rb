305ca87af338fc4b3656b905588107afbbc2be24
Yvonne Wangui <yveswangui@gmail.com>
2023-11-12 23:58:52 +0300

    test run

--- app.rb
+++ app.rb
@@ -4,4 +4,13 @@ if Rails.env.development?
     FileUtils.mkdir_p("/tmp/ruby-lsp-debug-sockets/")
     DEBUGGER__.open(nonstop: true, sock_path: "/tmp/ruby-lsp-debug-sockets/development.sock")
   end
-print "Hello World"
+
+print "Hello World" #everything on the same line
+puts "Hello Wangui" #on new line
+
+#Variables
+character_name = "John"
+character_age = "35"
+
+puts ("There was a man named " + character_name)
+puts ("He was "+ character_age)
